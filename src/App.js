71b343ef26528356258db647d71dd1efdddf1430
MarcelaSantos22 <ymarcela.quejada@udea.edu.co>
2017-05-05 15:26:21 -0500

    Prueba

--- src/App.js
+++ src/App.js
@@ -3,38 +3,29 @@ import logo from './airplane.svg';
 import './App.css';
 
 class App extends Component {
-    render() {
-        return ( <
-            div className = "App" >
-            <
-            div className = "App-header" >
-            <
-            img src = { logo }
-            className = "App-logo"
-            alt = "logo" / >
-            <
-            h2 > Desvolar < /h2> <
-            div className = "searchContent" >
-            <
-            div className = "searchField" >
-            <
-            label > Origen < /label> <
-            input type = "text" > < /input> <
-            /div> <
-            div className = "searchField" >
-            <
-            label > Destino < /label> <
-            input type = "text" > < /input> <
-            /div> <
-            div className = "searchButton" >
-            <
-            button > Buscar < /button> <
-            /div> <
-            /div> <
-            /div> <
-            /div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Desvolar</h2>
+          <div className="searchContent">
+            <div className="searchField">
+              <label>Origen</label>
+              <input type="text"></input>
+            </div>
+            <div className="searchField">
+              <label>Destino</label>
+              <input type="text"></input>
+            </div>
+            <div className="searchButton">
+              <button>Buscar</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
