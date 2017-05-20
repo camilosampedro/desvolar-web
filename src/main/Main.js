e46595083f63d8c4fec6b13742f8ab955f250a8e
JorgeBojaca <jorgebojaca@gmail.com>
2017-05-20 06:56:43 -0500

    States and functions to change its values

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,24 +1,69 @@
 import React, { Component } from 'react';
-import {Button, Input} from 'semantic-ui-react'
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
+import DatePicker from 'react-datepicker';
+import {Button, Input, Form} from 'semantic-ui-react'
+import 'react-datepicker/dist/react-datepicker.css';
 import './Main.css';
 import logo from '../airplane.svg';
+
+const cities=[{value:'BOG', text:'Bogotá'} ,{value:'MDE', text:'Medellín'} ]
 
 class Main extends Component {
   constructor(props){
     super(props);
     moment.locale('es');
+    moment(new Date()).format("DD/MM/YYYY");
     this.state = {
-      departureDate:  moment()
+      departureDate: moment(),
+      arrivalDate: '',
+      origin: '',
+      destination: '',
+      passengers: '1',
+      roundTrip: true,
     }
-    this.handleChange = this.handleChange.bind(this);
+    this.changeDepartureDate = this.changeDepartureDate.bind(this);
+    this.changeArrivalDate = this.changeArrivalDate.bind(this);
+    this.changeDestination = this.changeDestination.bind(this);
+    this.changeOrigin = this.changeOrigin.bind(this);
+    this.changePassengers = this.changePassengers.bind(this);
+    this.changeCheck = this.changeCheck.bind(this);
   }
-  handleChange(date){
-    console.info(date);
+  changeDestination(event){
     this.setState({
-      departureDate: date
+      destination: event.target.value
+    });
+  }
+  changeOrigin(event){
+    this.setState({
+      origin: event.target.value
+    });
+  }
+  changeDepartureDate(date){
+    if(date.isValid()){
+      this.setState({
+        departureDate: date
+      });
+    }
+  }
+  changeArrivalDate(date){
+    if(date.isValid()){
+      this.setState({
+        arrivalDate: date
+      });
+    }
+  }
+  changePassengers(event){
+    let aux = parseInt(event.target.value);
+    if(Number.isInteger(aux) && aux>0 && aux < 1000){
+      this.setState({
+        passengers: aux
+      });
+    }
+  }
+  changeCheck(event){
+    console.info( event.target.checked);
+    this.setState({
+      roundTrip: event.target.checked
     });
   }
 
@@ -28,33 +73,44 @@ class Main extends Component {
   }
 
   render() {
-    return (<div className="App">
+    return (
+    <div className="App">
       <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Desvolar.com</h2>
         <div className="searchContent">
           <div className="searchField">
             <label>Origen</label>
-            <Input placeholder="Ciudad origen..."></Input>
+            <select value={this.state.origin}  onChange={this.changeOrigin}>
+              <option value="" disabled>Ciudad origen...</option>
+              {cities.map(city=>(
+                <option value={city.value}>{city.text}</option>
+              ))}
+            </select>
           </div>
           <div className="searchField">
             <label>Destino</label>
-            <Input placeholder="Ciudad destino..."></Input>
+            <select value={this.state.destination}onChange={this.changeDestination}>
+              <option value="" disabled>Ciudad destino...</option>
+              {cities.map(city=>(
+                <option value={city.value}>{city.text}</option>
+              ))}
+            </select>
           </div>
           <div className="searchField">
             <label>Fecha Ida</label>
-            <DatePicker selected={this.state.departureDate} onChange={this.handleChange}/>
+            <DatePicker className="datePicker" selected={this.state.departureDate} onChange={this.changeDepartureDate}/>
           </div>
           <div className="searchField">
-            <label>Fecha Regreso</label>
-            <DatePicker/>
-          </div>
+                <label>Fecha Regreso</label>
+                <DatePicker className="datePicker" selected={this.state.arrivalDate} onChange={this.changeArrivalDate}/>
+                </div>
           <div className="searchField">
             <label>Número de pasajeros</label>
-            <input type="number" max="20" min="1"></input>
+            <input className="passengers" value={this.state.passengers} onChange={this.changePassengers}></input>
           </div>
           <div className="searchButton">
-            <Button onClick={this.submit.bind(this)}>Buscar</Button>
+            <button onClick={this.submit.bind(this)}>Buscar</button>
           </div>
         </div>
       </div>
