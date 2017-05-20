3eaeb8ae7cf17e449e30c88d6e52553f682cf16a
Camilo Sampedro <sampedro1903@gmail.com>
2017-05-19 21:19:22 -0500

    Main changed folder

--- src/App.js
+++ src/App.js
@@ -1,60 +1,15 @@
 import React, { Component } from 'react';
-import logo from './airplane.svg';
-import './App.css';
-import {Button, Input} from 'semantic-ui-react'
-import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker.css';
-import moment from 'moment';
+import Main from './main/Main'
 import {submitAllSearchs} from './service/search-service';
 
 class App extends Component {
   constructor(props){
-    super(props)
-    moment.locale('es');
-    this.state = {
-      departureDate:  moment()
-    }
-    this.handleChange = this.handleChange.bind(this);
+    super(props);
   }
-  handleChange(date){
-    console.info(date);
-    this.setState({
-      departureDate: date
-    })
-  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Desvolar.com</h2>
-          <div className="searchContent">
-            <div className="searchField">
-              <label>Origen</label>
-              <Input placeholder="Ciudad origen..."></Input>
-            </div>
-            <div className="searchField">
-              <label>Destino</label>
-              <Input placeholder="Ciudad destino..."></Input>
-            </div>
-            <div className="searchField">
-              <label>Fecha Ida</label>
-              <DatePicker selected={this.state.departureDate} onChange={this.handleChange}/>
-            </div>
-            <div className="searchField">
-              <label>Fecha Regreso</label>
-              <DatePicker/>
-            </div>
-            <div className="searchField">
-              <label>Número de pasajeros</label>
-              <input type="number" max="20" min="1"></input>
-            </div>
-            <div className="searchButton">
-              <Button>Buscar</Button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Main onChange={this.search.bind(this)}></Main>
     );
   }
 
