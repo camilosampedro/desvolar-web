78919dff6e6f562d67306eb0073dcd741b6e40dc
Camilo Sampedro <sampedro1903@gmail.com>
2017-05-19 23:40:16 -0500

    "Router" simulated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Main from './main/Main'
 import {submitAllSearchs} from './service/search-service';
 
+const MAIN_PHASE = 0;
+const RESULT_PHASE = 1;
+
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+    this.state = {
+      phase: MAIN_PHASE,
+      observable: {}
+    }
   }
 
   render() {
-    return (
-      <Main onChange={this.search.bind(this)}></Main>
-    );
+    if (this.state.phase == MAIN_PHASE) {
+      return (
+        <Main onChange={this.search.bind(this)}></Main>
+      );
+    } else {
+      // TODO: Change this div for "Result"
+      return (
+        <div></div>
+      );
+    }
   }
 
   search(filters) {
-    submitAllSearchs(filters);
+    this.setState({phase: RESULT_PHASE, observable: submitAllSearchs(filters)});
   }
+
 }
 
 export default App;
