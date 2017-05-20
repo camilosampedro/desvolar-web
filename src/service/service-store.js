f0cc1fb7696a96cf892abedfe7f6caf556981462
Camilo Sampedro <sampedro1903@gmail.com>
2017-05-20 09:47:53 -0500

    CHAN and  TOPA Urls added

--- src/service/service-store.js
+++ src/service/service-store.js
@@ -1,6 +1,6 @@
 // TODO: Update these URLs
 export const VIANCA_URL='URL';
-export const CHAN_URL='http://192.168.30.72:9000/';
-export const TOPA_URL='URL';
+export const CHAN_URL='//192.168.30.72:9000/chan/search';
+export const TOPA_URL='https://apirest-proyecto-empresariales.herokuapp.com/Search/';
 export const IBA_COLOMBIA_URL='https://b2wfgktwgl.execute-api.us-west-2.amazonaws.com/search/crudVuelos';
 export const TEST_URL='URL';
