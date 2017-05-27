56042674098183fe4702442a40f62f3c6597b746
JorgeBojaca <jorgebojaca@gmail.com>
2017-05-27 11:40:29 -0500

    Adjustment for TOPA_URL

--- src/service/service-store.js
+++ src/service/service-store.js
@@ -1,6 +1,6 @@
 // TODO: Update these URLs
 export const VIANCA_URL='https://vianca-api-dev.herokuapp.com/search';
 export const CHAN_URL='https://chanairlines-api.herokuapp.com/chan/search';
-export const TOPA_URL='https://apirest-proyecto-empresariales.herokuapp.com/Search';
+export const TOPA_URL='https://apirest-proyecto-empresariales.herokuapp.com/Search/';
 export const IBA_COLOMBIA_URL='https://b2wfgktwgl.execute-api.us-west-2.amazonaws.com/search/crudVuelos';
 export const TEST_URL='URL';
